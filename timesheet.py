7b32217db3055686b91012c5109a4bda2bf3c478
Quattro Musser <quattromusser@gmail.com>
2023-01-22 11:57:14 -0700

    can now use multiple functions without rerunning program

--- timesheet.py
+++ timesheet.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import time
 from classy import *
 import pickle as pickle # for long term storage
+import sys
 
 filename = "timesheet.pickle"
 
@@ -20,52 +21,64 @@ def myPickle(o, fn):
         pickle.dump(o, out_file, protocol=pickle.HIGHEST_PROTOCOL)
     return 0
 
-# get valid input
-first_in = input("(s)tart work \n(r)eport \n(o)verride mode \n> ")
-while first_in not in ["s", "r", "o"]:
-    first_in = input("invalid input \n(s)tart work \n(r)eport \n(o)verride mode \n> ")
+def main():
+    # get valid input
+    first_in = input("(s)tart work \n(r)eport \n(o)verride mode \n(q)uit \n> ")
+    while first_in not in ["s", "r", "o", "q"]:
+        first_in = input("invalid input \n(s)tart work \n(r)eport \n(o)verride mode \n(q)uit \n> ")
 
-# start work
-if first_in == "s":
-    start_time = time.time() # number of secounds from jan 1 1970 (start of unix time)
-    end_in = input("working... \n(e)nd work \n> ")
-    while end_in != "e":
-        end_in = input("invalid input \n(e)nd work \n> ")
-    hours_worked = (time.time() - start_time) / 60 / 60
-    # make new shift
-    shift = Shift(hours_worked)
-    sheet = myUnpickle(filename)
-    # add this shift to the sheet
-    sheet.addShift(shift)
-    # write the sheet back to file
-    myPickle(sheet, filename)
-    print(shift.report())
+    # start work
+    if first_in == "s":
+        start_time = time.time() # number of secounds from jan 1 1970 (start of unix time)
+        end_in = input("working... \n(e)nd work \n> ")
+        while end_in != "e":
+            end_in = input("invalid input \n(e)nd work \n> ")
+        hours_worked = (time.time() - start_time) / 60 / 60
+        # make new shift
+        shift = Shift(hours_worked)
+        sheet = myUnpickle(filename)
+        # add this shift to the sheet
+        sheet.addShift(shift)
+        # write the sheet back to file
+        myPickle(sheet, filename)
+        print(shift.report())
+        print("\n")
 
-# report mode
-elif first_in == "r":
-    # read the sheet from pickle
-    sheet = myUnpickle(filename)
-    sheet.report()
+    # report mode
+    elif first_in == "r":
+        # read the sheet from pickle
+        sheet = myUnpickle(filename)
+        sheet.report()
+        print("\n")
 
-# override mode
-elif first_in == "o":
-    # get sheet of times from pickle file
-    sheet = myUnpickle(filename)
-    sheet.report()
-    minus_hours = input("number of hours to remove: ")
-    while not minus_hours.replace(".", "").isnumeric():
-        minus_hours = input("real numbers only: ") 
-    # subtract minus hours from anywhere on the sheet
-    while minus_hours > 0:
-        sheet.shifts[0].hours -= minus_hours
-        # either we need to subtract from the next shift as well
-        if sheet.shifts[0].hours < 0:
-            minus_hours = sheet.shifts[0].hours * -1
-            del sheet.shifts[0]
-        # or we're done subtracting here
-        else:
-            minus_hours = 0
-    # write the sheet back to file
-    myPickle(sheet, filename)
-    print("done")
+    # override mode
+    elif first_in == "o":
+        # get sheet of times from pickle file
+        sheet = myUnpickle(filename)
+        sheet.report()
+        minus_hours = input("number of hours to remove: ")
+        while not minus_hours.replace(".", "").isnumeric():
+            minus_hours = input("real numbers only: ") 
+        # subtract minus hours from anywhere on the sheet
+        while minus_hours > 0:
+            sheet.shifts[0].hours -= minus_hours
+            # either we need to subtract from the next shift as well
+            if sheet.shifts[0].hours < 0:
+                minus_hours = sheet.shifts[0].hours * -1
+                del sheet.shifts[0]
+            # or we're done subtracting here
+            else:
+                minus_hours = 0
+        # write the sheet back to file
+        myPickle(sheet, filename)
+        print("done")
+        print("\n")
 
+    # quit
+    elif first_in == "q":
+        sys.exit(0)
+
+    # recur
+    main()
+
+main()
